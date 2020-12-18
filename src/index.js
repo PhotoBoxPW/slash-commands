0cf6d4cd1be17efe1155e0b3066b307fbcec8131
Snazzah <7025343+Snazzah@users.noreply.github.com>
2020-12-17 18:41:27 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const { SlashCreator, FastifyServer } = require('slash-create');
+const path = require('path');
+const CatLoggr = require('cat-loggr');
+const logger = new CatLoggr().setLevel(process.env.COMMANDS_DEBUG === 'true' ? 'debug' : 'info');
+
+const creator = new SlashCreator({
+  applicationID: process.env.DISCORD_APP_ID,
+  publicKey: process.env.DISCORD_PUBLIC_KEY,
+  token: process.env.DISCORD_BOT_TOKEN,
+  serverPort: 8020
+});
+
+creator.on('debug', (message) => logger.log(message));
+creator.on('warn', (message) => logger.warn(message));
+creator.on('error', (error) => logger.error(error));
+creator.on('synced', () => logger.info('Commands synced!'));
+creator.on('commandRan', (command, _, ctx) =>
+  logger.info(`${ctx.member.user.username}#${ctx.member.user.discriminator} (${ctx.member.id}) ran command ${command.commandName}`));
+creator.on('commandRegister', (command) =>
+  logger.info(`Registered command ${command.commandName}`));
+
+creator
+  .withServer(new FastifyServer())
+  .registerCommandsIn(path.join(__dirname, 'commands'))
+  .syncCommands()
+  .startServer();
+
+// This should serve in localhost:8020/interactions
