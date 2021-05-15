4e1b4af749f7d21f16163b027f817d65d526e6ef
Snazzah <suggesttosnazzy@gmail.com>
2021-05-14 21:06:16 -0500

    Load extractors on start

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { SlashCreator, FastifyServer } from 'slash-create';
 import path from 'path';
 import dotenv from 'dotenv';
+import { loadExtractors } from './media';
 
 let dotenvPath = path.join(process.cwd(), '.env');
 if (path.parse(process.cwd()).name === 'dist') dotenvPath = path.join(process.cwd(), '..', '.env');
@@ -30,24 +31,26 @@ creator
   .on('commandRegister', (command) => logger.info(`Registered command ${command.commandName}`))
   .on('commandError', (command, error) => logger.error(`Command ${command.commandName}:`, error));
 
-start().then(() => {
-  creator
-    .withServer(new FastifyServer())
-    .registerCommandsIn(path.join(__dirname, 'commands'))
-    .syncCommands()
-    .startServer();
+start()
+  .then(() => loadExtractors())
+  .then(() => {
+    creator
+      .withServer(new FastifyServer())
+      .registerCommandsIn(path.join(__dirname, 'commands'))
+      .syncCommands()
+      .startServer();
 
-  if (process.env.TEST_GUILD) {
-    const alreadySynced = !!creator.commands.find(
-      (command) => command.guildIDs && command.guildIDs.includes(process.env.TEST_GUILD)
-    );
+    if (process.env.TEST_GUILD) {
+      const alreadySynced = !!creator.commands.find(
+        (command) => command.guildIDs && command.guildIDs.includes(process.env.TEST_GUILD)
+      );
 
-    if (!alreadySynced)
-      creator
-        .syncCommandsIn(process.env.TEST_GUILD)
-        .then(() => logger.info(`Synced test guild!`))
-        .catch((e) => logger.error(`Failed to sync test guild!`, e));
-  }
-});
+      if (!alreadySynced)
+        creator
+          .syncCommandsIn(process.env.TEST_GUILD)
+          .then(() => logger.info(`Synced test guild!`))
+          .catch((e) => logger.error(`Failed to sync test guild!`, e));
+    }
+  });
 
 // This should serve in localhost:8020/interactions
